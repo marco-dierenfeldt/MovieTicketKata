93b6d20a4fbdf5d3f0294419ff21e75986088136
Marco Dierenfeldt <marco.dierenfeldt@meditec.zeiss.com>
2011-05-06 10:18:22 +0200

    refactored comments and renamed constants and implemented re-initialisation

--- src/de/saxsys/dojo/ticketkata/CinemaCashRegister.java
+++ src/de/saxsys/dojo/ticketkata/CinemaCashRegister.java
@@ -21,62 +21,88 @@ package de.saxsys.dojo.ticketkata;
  *
  * @author Marco Dierenfeldt
  * @version 0.1
+ * @see TicketKataInterface
  */
 public class CinemaCashRegister implements TicketKataInterface {
 
-    private final float generalAdmissionFee = 11.00f;
-    private final float studentsFee = 8.00f;
-    private final float seniorCitizensFee = 6.00f;
-    private final float childrenFee = 5.50f;
-    private final float groupFee = 6.00f;
-    private final float movie3dAddition = 3.00f;
-    private final float overlengthAddition = 1.50f;
-    private final float movieDayResidue = 2.00f;
-    private final float weekendAddition = 1.50f;
-    private final float logeAddition = 2.00f;
-    private final int overlengthLimit = 120;
-    private final int seniorAgeLimit = 64;
-    private final int childrenAgeLimit = 13;
-    private final int groupLimit = 20;
-    private final Day movieDay = Day.THU;
+    //defining the constant values for the cashregister
+    private final float GENERAL_ADMISSION_FEE = 11.00f;
+    private final float STUDENTS_FEE = 8.00f;
+    private final float SENIOR_CITIZENS_FEE = 6.00f;
+    private final float CHILDREN_FEE = 5.50f;
+    private final float GROUP_FEE = 6.00f;
+    private final float MOVIE_3D_ADDITION = 3.00f;
+    private final float OVERLENGTH_ADDITION = 1.50f;
+    private final float MOVIE_DAY_RESIDUE = 2.00f;
+    private final float WEEKEND_ADDITION = 1.50f;
+    private final float LOGE_ADDITION = 2.00f;
+    private final int OVERLENGTH_LIMIT = 120;
+    private final int SENIOR_AGE_LIMIT = 64;
+    private final int CHILDREN_AGE_LIMIT = 13;
+    private final int GROUP_LIMIT = 20;
+    private final Day MOVIE_DAY = Day.THU;
+
+
     private Day day = null;
     private float priceModificator = 0.0f;
     private float sum = 0.0f;
     private int numberOfTickets = 0;
 
+    /**
+     * (1) New customers arrive at your ticket booth and tell you
+     * what movie they'd like to see (so keep it in mind ;-)
+     *
+     * @param runtime       movie's runtime in minutes
+     * @param day           day of the week (enum)
+     * @param isParquet     true if seating category is 'parquet' (and not 'loge')
+     * @param is3D          true if the movie's shown in 3D
+     */
     public void startPurchase(int runtime, Day day, boolean isParquet, boolean is3D) {
+        /* re-init numberOfTickets, sum and priceModificator because this
+           object could be used multiple times */
+        numberOfTickets = 0;
+        priceModificator = 0.0f;
+        sum = 0.0f;
+        
         this.day = day;
 
+        //calculate the priceModificator
         if (is3D) {
-            priceModificator += movie3dAddition;
+            priceModificator += MOVIE_3D_ADDITION;
         }
         if (!isParquet) {
-            priceModificator += logeAddition;
+            priceModificator += LOGE_ADDITION;
         }
-        if (day.equals(movieDay)) {
-            priceModificator -= movieDayResidue;
+        if (day.equals(MOVIE_DAY)) {
+            priceModificator -= MOVIE_DAY_RESIDUE;
         }
         if (day.equals(Day.SAT) || day.equals(Day.SUN)) {
-            priceModificator += weekendAddition;
+            priceModificator += WEEKEND_ADDITION;
         }
-        if (runtime > overlengthLimit) {
-            priceModificator += overlengthAddition;
+        if (runtime > OVERLENGTH_LIMIT) {
+            priceModificator += OVERLENGTH_ADDITION;
         }
     }
 
+    /**
+     * (2) Add a ticket to the customers' bill
+     *
+     * @param age           the age of the ticket buyer in years
+     * @param isStudent     true if the ticket buyer is a student
+     */
     public void addTicket(int age, boolean isStudent) {
         numberOfTickets++;
 
-        if (isStudent && age <= seniorAgeLimit && age >= childrenAgeLimit) {
-            sum += studentsFee;
+        if (isStudent && age <= SENIOR_AGE_LIMIT && age >= CHILDREN_AGE_LIMIT) {
+            sum += STUDENTS_FEE;
         } else {
-            if (age > seniorAgeLimit) {
-                sum += seniorCitizensFee;
+            if (age > SENIOR_AGE_LIMIT) {
+                sum += SENIOR_CITIZENS_FEE;
             } else {
-                if (age < childrenAgeLimit) {
-                    sum += childrenFee;
+                if (age < CHILDREN_AGE_LIMIT) {
+                    sum += CHILDREN_FEE;
                 } else {
-                    sum += generalAdmissionFee;
+                    sum += GENERAL_ADMISSION_FEE;
                 }
             }
         }
@@ -84,13 +110,18 @@ public class CinemaCashRegister implements TicketKataInterface {
         sum += priceModificator;
     }
 
+    /**
+     * (3) Calculate the total admission for the current customer(s)
+     *
+     * @return  total in dollars
+     */
     public float finishPurchase() {
-        if (numberOfTickets >= groupLimit) {
-            if (day.equals(movieDay)) {
+        if (numberOfTickets >= GROUP_LIMIT) {
+            if (day.equals(MOVIE_DAY)) {
                 //no residue on moviedays for groups
-                priceModificator += movieDayResidue;
+                priceModificator += MOVIE_DAY_RESIDUE;
             }
-            float groupSum = (groupFee + priceModificator) * numberOfTickets;
+            float groupSum = (GROUP_FEE + priceModificator) * numberOfTickets;
             sum = (groupSum < sum) ? groupSum : sum;
         }
 
